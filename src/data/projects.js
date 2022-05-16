abc88dbbfe253ab20cfc9692494f540c6e0c7df1
ausamindec <ausamindec@gmail.com>
2022-05-16 16:52:00 +0530

    Adding data to be displayed for projects

---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+        projectName: " tech-blog",
+        projectIcon: "tech-blog",
+        projectDesc: `A web application where user can add blogs`,
+        gitHubLink: "https://github.com/ausamindec/tech-blog",
+        appLink: "https://timpass-blog.herokuapp.com/"
+    },
+    {
+        projectName: "Note Taker",
+        projectIcon: "note-taker",
+        projectDesc: `A Note Taker application where user can take notes.`,
+        gitHubLink: "https://github.com/ausamindec/Week-11-NoteTaker",
+        appLink: "https://aminnotetaker.herokuapp.com/"
+    },
+    {
+        projectName: "Book Search",
+        projectIcon: "book-search",
+        projectDesc: `An app where user can search books and mark as there favourite.`,
+        gitHubLink: "https://github.com/ausamindec/bookSearchEngine",
+        appLink: "https://search-book-eng.herokuapp.com/"
+    },
+    {
+        projectName: "Text Editor",
+        projectIcon: "text-editor",
+        projectDesc: `A simple text editor app that can function online. Needed to use a variety of data persistence options in order to ensure that data is not lost in any scenario. The application first looks to use the data in the indexedDB to populate the editor, then if it cannot access that it will use local storage.`,
+        gitHubLink: "https://github.com/ausamindec/PWA-textEditor",
+        appLink: "https://aste-text.herokuapp.com"
+    },
+    {
+        projectName: "Flight Logger",
+        projectIcon: "flight-logger",
+        projectDesc: `An app where user can log flight details. Created using node and handlebars.`,
+        gitHubLink: "https://github.com/ausamindec/flightLogs",
+        appLink: "https://proje-2.herokuapp.com/"
+    },
+];
